d69d2189b299c1fbccf1afbb048435d0b7aba86b
Archon033 <144591435+Archon033@users.noreply.github.com>
2025-03-06 17:36:59 +0800

    Delete src/main/java directory

--- src/main/java/aa.java
+++
@@ -1,7 +0,0 @@
-public class aa {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Welcome");
-        System.out.println("Archon");
-    }
-}
